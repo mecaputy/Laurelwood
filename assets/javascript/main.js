66d06ee2667268277cc0a0007a8e686daa0f7314
mecaputy <martina.e.caputy@gmail.com>
2018-05-30 22:13:04 -0400

    third

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
   });
-var instance = M.Parallax.getInstance(elem);
+// var instance = M.Parallax.getInstance(elem);
